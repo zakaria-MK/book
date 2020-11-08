a1591e1454f728c0b6d2875ed06df1fc157164b6
Zakaria Mekelleche <zmekell@gmail.com>
2020-11-08 15:36:23 +0100

    add writers section using bootstrap with jquery to collapse and show books foreach writer

--- js/script.js
+++ js/script.js
@@ -3,4 +3,24 @@ $(document).ready(function () {
     'resizeDuration': 200,
     'wrapAround': true
   })
+
+  $(window).scroll(function () {
+    let position = $(this).scrollTop();
+    console.log(position);
+    if (position >= 200) {
+      $('.line1').addClass('fromleft');
+      $('.line2').addClass('fromright');
+    } else {
+      $('.line1').removeClass('fromleft');
+      $('.line2').removeClass('fromright');
+    }
+  })
+
+  $('.writters').click(function (event) {
+    
+    if (event.target.id.split('-')[0] === 'button') {
+      $('#book-1').attr('src', '../img/writers/' + event.target.id.split('-')[1] + '-book1.jpg');
+      $('#book-2').attr('src', '../img/writers/' + event.target.id.split('-')[1] + '-book2.jpg');
+    }
+  })
 })
